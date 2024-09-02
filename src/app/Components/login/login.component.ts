c4452798ae9d1fa96c3e41a94ead35e0b81299a7
Pramod J M <pramodjm4@gmail.com>
2024-09-02 17:47:27 +0530

    Added login model and home Banner

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+declare var google: any;
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent {
-
+export class LoginComponent implements OnInit {
+  ngOnInit(): void {
+    google.account.id.initialize({
+      client_id: '',
+      callback: (resp: any) => {},
+    });
+    google.account.id.renderButton(document.getElementById('google-button'));
+  }
 }
