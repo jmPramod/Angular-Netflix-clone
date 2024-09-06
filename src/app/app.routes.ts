6f74e1bbbb484074dee54cdfb59c6f8f39f377df
Pramod JM <pramodjm4@gmail.com>
2024-09-06 07:38:05 +0530

    added page and core folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,14 +4,14 @@ export const routes: Routes = [
   {
     path: '',
     loadComponent: () =>
-      import('../app/Components/login/login.component').then(
+      import('./pages/login/login.component').then(
         (a) => a.LoginComponent
       ),
   },
   {
     path: 'browse',
     loadComponent: () =>
-      import('../app/Components/browse/browse.component').then(
+      import('./pages/browse/browse.component').then(
         (a) => a.BrowseComponent
       ),
   },
