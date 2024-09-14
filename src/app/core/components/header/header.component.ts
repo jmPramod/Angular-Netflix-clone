5b6d525363b132d84d05af147a95f56e202b5e5f
Pramod JM <pramodjm4@gmail.com>
2024-09-14 11:54:53 +0530

    added footer and header.

---
+++ src/app/core/components/header/header.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  @Input({required: true}) userImage:string=""
+navList=["home","TV Shows","News & Popular","My List","Browse By language"]
+}
--- src/app/pages/browse/browse.component.ts
+++ src/app/pages/browse/browse.component.ts
@@ -1,10 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../shared/services/auth.service';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../../core/components/header/header.component';
 
 @Component({
   selector: 'app-browse',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,HeaderComponent],
   templateUrl: './browse.component.html',
   styleUrl: './browse.component.css'
 })
